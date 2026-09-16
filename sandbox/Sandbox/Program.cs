55c354ec88307b124eede661110183eabeab704c
Jason Jones <jj180@byui.edu>
2026-09-16 09:37:46 -0600

    Dinosaur added

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Bonjour bagette!");
+        Console.WriteLine("Bonjour bagette!"); 
+        Console.WriteLine("I like Dinosaurs!");
     }
 } 
