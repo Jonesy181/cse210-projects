3a4450d2e97e3eb42bcacef12f1e5e73331a8f09
Jason Jones <jj180@byui.edu>
2026-09-23 09:48:02 -0600

    function

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,9 +2,26 @@ using System;
 
 class Program
 {
+
+    static double AddNumbers(double x, int y)
+        {
+            return x +y;
+        }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Bonjour bagette!"); 
-        Console.WriteLine("I like Dinosaurs!");
+       // for(int i = 0; i < 105; i+=5)
+        //{
+          //  Console.WriteLine(i);
+        //}
+
+        // List<string> myFriends = new List<string> {"Bob", "Betty", "Bill", "Bubba"};
+        // myFriends.Add("James");
+        // myFriends.Add("Doug");
+
+        // foreach(string name in myFriends)
+        // {
+        //     Console.WriteLine(name);
+        // }  
     }
-} 
+}
